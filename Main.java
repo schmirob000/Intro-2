59ec4a9dc6d44c7051aa4eb7af3f3eba99d9ece7
Jack Stratton <jack@phroa.net>
2015-11-18 17:43:54 -0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * Example class to get people in the spirit of GitHub.
+ *
+ * Try it out yourself! You'll need the Java JDK, or development tools, set up
+ * on your computer. Open up a terminal or command prompt in the folder where
+ * you saved this file, and run "javac Intro.java" followed by "java Intro" if
+ * "javac" didn't produce any errors.
+ */
+public class Main {
+
+    /**
+     * The "main()" method is where Java knows to start our program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+    }
+}
